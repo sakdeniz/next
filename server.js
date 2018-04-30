caa9af9dafbaacdbe6a00b5db37243ff6ff18f86
sakdeniz <38104512+sakdeniz@users.noreply.github.com>
2018-04-30 11:50:57 +0300

    update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var qs = require('querystring');
 var server;
 const crypto=require('crypto');
 const Client = require('bitcoin-core');
-const appDataPath=process.env.APPDATA+"\\NavCoin4";
+const appDataPath=process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences/Navcoin4' : '/var/local/NavCoin4');
 const fileAddressBook=appDataPath+"\\addressbook.dat";
 const fileWalletPassword=appDataPath+"\\walletpass.dat";
 
