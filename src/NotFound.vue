5bb5026bf364a4f3f892594a9ab632744dfeb7ff
Ro Savage <rowan.savage@gmail.com>
2018-05-02 23:00:11 +1200

    Updated some of the processes when starting in dev mode

---
+++ src/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="center">
+  <p>
+    RPC password and port are missing.
+    Please reload the page with <pre><b>?rpcpassword=[password]&rpcport=[port]</b></pre>
+  </p>
+</div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="css">
+  .center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
+import NotFound from './NotFound.vue'
 import LightBootstrap from './light-bootstrap-main'
 import SuiVue from 'semantic-ui-vue';
 import routes from './routes/routes'
@@ -15,13 +16,28 @@ const router = new VueRouter({
 window.hostname='http://localhost:3000/';
 var rpcpassword=document.URL.match(/rpcpassword=([0-9A-Za-z]+)/)
 var rpcport=document.URL.match(/rpcport=([0-9]+)/)
-window.token=rpcpassword[1];
-window.rpcport=rpcport[1];
-window.config={headers: {'Content-Type': 'application/x-www-form-urlencoded'},responseType: 'text'};
-var vm = new Vue({
-  el: '#app',
-  data:{},
-  methods:{}, 
-  render: h => h(App),
-  router
-})
+if (rpcpassword && rpcport) {
+  window.token=rpcpassword[1];
+  window.rpcport=rpcport[1];
+  window.config={headers: {'Content-Type': 'application/x-www-form-urlencoded'},responseType: 'text'};
+}
+
+var vm
+
+if (!rpcpassword || !rpcport) {
+  vm = new Vue({
+    el: '#app',
+    data:{},
+    methods:{},
+    render: h => h(NotFound),
+    router
+  })
+} else {
+  vm = new Vue({
+    el: '#app',
+    data:{},
+    methods:{},
+    render: h => h(App),
+    router
+  })
+}
