f9f78b6801a47ae9842749510b7caca1890a8caf
Dan Tudor <dtudor01@gmail.com>
2018-06-01 17:55:35 +0100

    Update URL for nav explorer

--- src/components/Dashboard/Views/BlockExplorer.vue
+++ src/components/Dashboard/Views/BlockExplorer.vue
@@ -2,7 +2,7 @@
 <div class="content">
   <div class="container-fluid">
     <h4 class="card-title"><i class="ion-android-search"></i> Block Explorer</h4>
-    <a class="btn btn-fill btn-info" target="_blank" href="http://alpha.navexplorer.com/"><i class="ion-android-open"></i>&nbsp;NAV Explorer</a>
+    <a class="btn btn-fill btn-info" target="_blank" href="https://www.navexplorer.com/"><i class="ion-android-open"></i>&nbsp;NAV Explorer</a>
     <a class="btn btn-fill btn-info" target="_blank" href="https://chainz.cryptoid.info/nav/"><i class="ion-android-open"></i>&nbsp;CryptoID</a>
   </div>
 </div>
