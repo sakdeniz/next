df5aae8aac2fa1836c0ddacf8a90ffe6e396aacd
sakdeniz <38104512+sakdeniz@users.noreply.github.com>
2018-06-09 20:36:18 +0300

    fix

--- main.js
+++ main.js
@@ -315,7 +315,7 @@ function createMainWindow ()
 		if (line=="next:open-data-folder")
 		{
 			var path=appDataPath;
-			path=path.replace(' ', '\ ');
+			path=path.replace(/ /g, '\\ ');
 			console.log("Open data folder:"+path);
 			shell.openItem(path);
 		}
